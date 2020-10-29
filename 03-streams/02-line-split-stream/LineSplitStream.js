9e9a1e4ae50ebd3a796eafba00a82bf8a4b94a71
Alexandra <alex-vorobeva@yandex-team.ru>
2020-10-29 19:36:29 +0300

    line spliit script

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,29 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.current = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const _chunk = chunk.toString('utf8');
+    this.current += _chunk;
+    const chunksArray = this.current.split(os.EOL);
+    const length = chunksArray.length;
+    const lastElem = chunksArray.pop();
+
+    if (length > 1) {
+      for (const item of chunksArray) {
+        this.push(item);
+      }
+    }
+  
+    this.current = lastElem;
+    callback();
   }
 
   _flush(callback) {
+    this.push(this.current);
+    callback();
   }
 }
 
