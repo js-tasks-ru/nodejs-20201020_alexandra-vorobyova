26788009f9c3b648bc0a9da2acd4efbe53a3ddff
Alexandra <alex-vorobeva@yandex-team.ru>
2020-11-03 18:48:36 +0300

    delete file

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,16 +1,37 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const { existsSync, unlink } = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
+  const parsedUrl = pathname.split('/');
+  
+  if (parsedUrl && parsedUrl.length > 1) {
+    res.statusCode = 400;
+    return res.end('Nested path detected');
+  }
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  if (!existsSync(filepath)) {
+    res.statusCode = 404;
+    return res.end('File doesnt exist');
+  }
+
   switch (req.method) {
     case 'DELETE':
+      unlink(filepath, (err) => {
+        if (err) {
+          res.statusCode = 501;
+          return res.end('Error');
+        }
+
+        res.statusCode = 200;
+        return res.end('Success');
+      });
 
       break;
 
