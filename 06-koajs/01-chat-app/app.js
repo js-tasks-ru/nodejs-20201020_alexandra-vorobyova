903b0b605e8e49ec9ad49cc2f98208c1da2802b2
Alexandra <alex-vorobeva@yandex-team.ru>
2020-11-05 12:47:04 +0300

    koajs long polling

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,32 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let promises = [];
+
 router.get('/subscribe', async (ctx, next) => {
+    const message = await new Promise((resolve) => {
+        promises.push(resolve);
+    });
+    ctx.status = 200;
+    ctx.body = message;
+    return next();
 });
 
 router.post('/publish', async (ctx, next) => {
+    const message = ctx.request.body.message;
+
+    if (!message) {
+        return next();
+    }
+
+    for await (const p of promises) {
+        p(ctx.request.body.message);
+    }
+
+    promises = [];
+    ctx.status = 201;
+    ctx.body = 'ok';
+    return next();
 });
 
 app.use(router.routes());
