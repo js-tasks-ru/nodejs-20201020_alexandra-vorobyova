1550773763390c62d64da387a6bbd4ee25bd3f77
Alexandra <alex-vorobeva@yandex-team.ru>
2020-11-19 21:39:12 +0300

    fix

--- 08-authentication/01-local/libs/strategies/local.js
+++ 08-authentication/01-local/libs/strategies/local.js
@@ -3,25 +3,23 @@ const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    function(email, password, done) {
+    async function(email, password, done) {
 
-      return User.findOne({ email }, async function(err, user) {
-        if (err) { 
-          return done(null, false, 'Стратегия подключена, но еще не настроена'); 
-        }
+      try {
+        const user = await User.findOne({ email });
 
         if (!user) {
           return done(null, false, 'Нет такого пользователя');
         }
 
         const isValidPassword = await user.checkPassword(password);
-
         if (!isValidPassword) {
           return done(null, false, 'Неверный пароль');
         }
 
-        return done(null, user);
-      })
-      
+        done(null, user);
+      } catch(err) {
+        done(err);
+      }
     },
 );
